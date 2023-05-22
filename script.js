9f1abbd2b0c2c871318837f916ce357276249620
amir <amir@dragonchain.com>
2023-05-22 22:35:01 +0100

    add under dev sign

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ const playSound = (e) => {
 soundBtn.addEventListener("click", function (e) {
   playSound(e);
 });
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("mouseover", () => {
+    btn.style.color = "red";
+    btn.innerText = "Under Dev";
+  });
+  btn.addEventListener("mouseleave", () => {
+    btn.style.color = "white";
+    btn.innerText = "Know more";
+  });
+});
